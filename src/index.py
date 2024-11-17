045fd88b451d6b24309e9cda29e7feb734071f81
Germuu <jeremyyamajako@gmail.com>
2024-11-17 17:08:27 +0200

    boilerbranch

--- src/index.py
+++ src/index.py
@@ -1,10 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5001, host="0.0.0.0", debug=True)

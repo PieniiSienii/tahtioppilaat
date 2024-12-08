3231df9006d8279daf39f370f021765f426f5e3e
kuussant <santeri.kuusisto@helsinki.fi>
2024-12-08 19:30:50 +0200

    Added append_bibtex_entry

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 import bibtexparser
+import subprocess
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 
@@ -47,3 +48,31 @@ def create_bibtex(entrytype, contents):
 
     return bibtex_string
 
+
+def convert_to_bibtex(doi):
+    # Use subprocess to call the 'doi2bib' command
+    try:
+        result = subprocess.run(
+            ['doi2bib', doi],
+            check=True,  # Raise an exception if the command fails
+            stdout=subprocess.PIPE,  # Capture the standard output
+            stderr=subprocess.PIPE  # Capture the standard error
+        )
+        # Return the BibTeX result
+        return result.stdout.decode('utf-8')  # Convert bytes to string
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e.stderr.decode('utf-8')}")
+        return None
+
+
+def append_bibtex_entry(file_path, entry_str):
+    with open(file_path, 'r') as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file)
+
+    new_database = bibtexparser.loads(entry_str)
+
+    bib_database.entries.extend(new_database.entries)
+
+    writer = BibTexWriter()
+    with open(file_path, 'w') as bibtex_file:
+        bibtex_file.write(writer.write(bib_database))
